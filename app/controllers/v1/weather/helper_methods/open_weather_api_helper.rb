dda8708d8ed77a67e4f96986e0ea3062e4d12f18
Prangshuman <temp@example.com>
2020-03-16 19:25:31 +0100

    few fixes

--- app/controllers/v1/weather/helper_methods/open_weather_api_helper.rb
+++ app/controllers/v1/weather/helper_methods/open_weather_api_helper.rb
@@ -1,18 +1,16 @@
-# require 'date'
-# require 'httparty'
 class V1::Weather::HelperMethods::OpenWeatherApiHelper
   include HTTParty
   base_uri 'api.openweathermap.org/'
 
+  API_KEY = 'e036a0a554b6751c3b3288348997f030'.freeze
+  private_constant :API_KEY
+
   def weather(loc_id)
-    # TODO: make these private
-    api_key = 'e036a0a554b6751c3b3288348997f030'
     s_url = '/data/2.5/forecast?'
     s_id = 'id='
     s_appid = '&appid='
     s_unit = '&units=metric'
-
     # Default temperature unit is celcius
-    self.class.get(s_url + s_id + loc_id.to_s + s_appid + api_key + s_unit)
+    self.class.get(s_url + s_id + loc_id.to_s + s_appid + API_KEY + s_unit)
   end
 end
